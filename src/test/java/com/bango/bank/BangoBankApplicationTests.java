abbb814d1a5187f037b13d8fc3141f90bec971cf
Esteban Eduardo Garcia Ojeda <esteban.e.garcia.o@gmail.com>
2025-03-03 23:32:27 -0600

    Transaction callback

--- src/test/java/com/bango/bank/BangoBankApplicationTests.java
+++ src/test/java/com/bango/bank/BangoBankApplicationTests.java
@@ -1,7 +1,13 @@
 package com.bango.bank;
 
+import static com.bango.bank.util.CommonObjects.getTransaction;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bango.bank.config.TransactionCallback;
+import com.bango.bank.entities.Transaction;
 
 @SpringBootTest
 class BangoBankApplicationTests {
@@ -11,4 +17,12 @@ class BangoBankApplicationTests {
 		BangoBankApplication.main(new String[] {});
 	}
 
+	@Test
+	void onBeforeConvert() {
+		TransactionCallback transactionCallback = new TransactionCallback();
+
+		Transaction transaction = transactionCallback.onBeforeConvert(getTransaction(), "");
+
+		Assertions.assertEquals("retiro", transaction.getType());
+	}
 }
